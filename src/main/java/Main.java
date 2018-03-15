06a510efc87d84159a334ac8027d5784b731536d
Yuhuan Qiu <yq56@cornell.edu>
2018-03-14 20:42:35 -0500

    fix spark logger issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import io.github.ccincharge.newsapi.NewsApi;
+
+import static spark.Spark.get;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // http://localhost:4567/hello
+        get("/hello", (req, res) -> "Hello World");
     }
 }
